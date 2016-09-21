8d10fd2f85c92f595a974cf3df94ca84f73de66f
Robert Simmons <robsimm@gmail.com>
2016-09-21 22:53:38 +0100

    Implementing method signatures for contacts reader

--- src/AddressProcessor/Contacts/ContactsReader.cs
+++ src/AddressProcessor/Contacts/ContactsReader.cs
@@ -1,10 +1,26 @@
 ﻿namespace AddressProcessing.Contacts
 {
     using System;
+    using System.IO;
     using System.Threading.Tasks;
 
     class ContactsReader : IContactsReader
     {
+        private StreamReader streamReader = null;
+        private char[] tabSeperator;
+
+        public ContactsReader(string fileName)
+            : this(File.OpenText(fileName), new char[] { '\t' })
+        {
+
+        }
+
+        public ContactsReader(StreamReader streamReader, char[] tabSeperator)
+        {
+            this.streamReader = streamReader;
+            this.tabSeperator = tabSeperator;
+        }
+
         public void Close()
         {
             throw new NotImplementedException();
