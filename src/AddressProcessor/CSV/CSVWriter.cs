cfe4fb69aac2a28779a50c4eae9f97d2cea901d0
Robert Simmons <robsimm@gmail.com>
2016-09-21 22:46:04 +0100

    Creation of interface and impl for CSV writer - To uncouple current single responsibility issues

---
+++ src/AddressProcessor/CSV/CSVWriter.cs
@@ -0,0 +1,23 @@
+﻿namespace AddressProcessing.CSV
+{
+    using System;
+    using System.Threading.Tasks;
+
+    class CSVWriter : ICSVWriter
+    {
+        public void Close()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task WriteToCSV(params string[] columnCollection)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/AddressProcessor/CSV/ICSVWriter.cs
@@ -0,0 +1,12 @@
+﻿namespace AddressProcessing.CSV
+{
+    using System;
+    using System.Threading.Tasks;
+
+    interface ICSVWriter : IDisposable
+    {
+        Task WriteToCSV(params string[] columnCollection);
+
+        void Close();
+    }
+}
