cf789432867cf45fd7c0663e76fe7ded1fb7aaea
Robert Simmons <robsimm@gmail.com>
2016-09-21 23:25:43 +0100

    Implement class constructors for CSVWriter

--- src/AddressProcessor/CSV/CSVWriter.cs
+++ src/AddressProcessor/CSV/CSVWriter.cs
@@ -2,9 +2,29 @@
 {
     using System;
     using System.Threading.Tasks;
+    using System.IO;
 
-    class CSVWriter : ICSVWriter
+    public class CSVWriter : ICSVWriter
     {
+        private string lineSeperator = string.Empty;
+        private StreamWriter streamWriter = null;
+
+        public CSVWriter(string outputFileName, string seperator)
+            : this(CreateWriteStream(outputFileName), seperator)
+        {
+        }
+
+        public CSVWriter(StreamWriter streamWriter, string seperator)
+        {
+            this.streamWriter = streamWriter;
+            lineSeperator = seperator;
+        }
+
+        private static StreamWriter CreateWriteStream(string outputFileName)
+        {
+            throw new NotImplementedException();
+        }
+
         public void Close()
         {
             throw new NotImplementedException();
