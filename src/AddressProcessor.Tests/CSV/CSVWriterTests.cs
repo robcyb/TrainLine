912f6c2ad67e933974cdf3f42753f4468c9f2825
Robert Simmons <robsimm@gmail.com>
2016-09-22 14:51:00 +0100

    Tests for file and directory not exist

--- src/AddressProcessor.Tests/CSV/CSVWriterTests.cs
+++ src/AddressProcessor.Tests/CSV/CSVWriterTests.cs
@@ -1,22 +1,42 @@
 ﻿namespace AddressProcessing.Tests.CSV
 {
+    using AddressProcessing.CSV;
     using NUnit.Framework;
+    using System.Globalization;
+    using System.IO;
+    using Utils;
 
     [TestFixture]
     public class CSVWriterTests
     {
         private const string TestDataOutputDirectory = "output";
+        private const string NewLineSeperator = "\n";
 
         [Test]
         public void Should_write_new_file_when_not_exist()
         {
-            Assert.Inconclusive();
+            string fileNotExist = @"output\notexist.csv";
+
+            TestUtils.DeleteFile(fileNotExist);
+
+            using (new CSVWriter(fileNotExist, NewLineSeperator))
+            {
+                Assert.True(File.Exists(fileNotExist));
+            }
         }
 
         [Test]
         public void Should_create_directory_when_not_exist()
         {
-            Assert.Inconclusive();
+            string directory = "notexists";
+            string directoryNotExist = string.Format(CultureInfo.InvariantCulture, @"{0}\notfound.csv", directory);
+
+            TestUtils.DeleteDirectory(directoryNotExist); // make sure it definately doesn't exist!
+
+            using (new CSVWriter(directoryNotExist, NewLineSeperator))
+            {
+                Assert.True(Directory.Exists(directory));
+            }
         }
 
         [Test]
