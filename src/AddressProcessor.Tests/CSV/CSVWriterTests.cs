f459fdc1614af355eb40e9717ecc4361fdbbf85a
Robert Simmons <robsimm@gmail.com>
2016-09-22 12:14:50 +0100

    Creating skeleton tests for CSVWriter and ContactsReader classes

---
+++ src/AddressProcessor.Tests/CSV/CSVWriterTests.cs
@@ -0,0 +1,40 @@
+﻿namespace AddressProcessing.Tests.CSV
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class CSVWriterTests
+    {
+        private const string TestDataOutputDirectory = "output";
+
+        [Test]
+        public void Should_write_new_file_when_not_exist()
+        {
+            Assert.Inconclusive();
+        }
+
+        [Test]
+        public void Should_create_directory_when_not_exist()
+        {
+            Assert.Inconclusive();
+        }
+
+        [Test]
+        public void Should_write_to_file_correctly()
+        {
+            Assert.Inconclusive();
+        }
+
+        [Test]
+        public void Should_use_a_streamwriter_to_write_file()
+        {
+            Assert.Inconclusive();
+        }
+
+        [TestFixtureTearDown]
+        public void TearDown()
+        {
+
+        }
+    }
+}
---
+++ src/AddressProcessor.Tests/Contacts/ContactsReaderTests.cs
@@ -0,0 +1,38 @@
+﻿namespace AddressProcessing.Tests.Contacts
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class ContactsReaderTests
+    {
+        [Test]
+        public void Should_use_a_streamreader_to_read_file()
+        {
+            Assert.Inconclusive();
+        }
+
+        [Test]
+        public void Should_read_from_a_file()
+        {
+            Assert.Inconclusive();
+        }
+
+        [Test]
+        public void Should_throw_directorynotfoundexception_when_directory_not_exist()
+        {
+            Assert.Inconclusive();
+        }
+
+        [Test]
+        public void Should_throw_filenotfoundexception_when_file_not_exist()
+        {
+            Assert.Inconclusive();
+        }
+
+        [TestFixtureTearDown]
+        public void TearDown()
+        {
+
+        }
+    }
+}
